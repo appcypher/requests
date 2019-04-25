b4962dab5d432e65398a31e38559b086f1ea83cd
Steve Akinyemi <appcypher@outlook.com>
2019-04-25 01:55:00 +0100

    Fix request list UI

--- client/components/sidebar/sidebar.component.js
+++ client/components/sidebar/sidebar.component.js
@@ -7,6 +7,13 @@ import messageIcon from '../../assets/icons/message.svg';
 import requestIcon from '../../assets/icons/request.svg';
 import calendarIcon from '../../assets/icons/calendar.svg';
 import statsIcon from '../../assets/icons/stats.svg';
+import { getStaff } from '../../http';
+import avatarUrl from '../../assets/icons/placeholder.png';
+import {
+  hideLoader,
+  showLoader,
+  showError,
+} from '../app/app.component';
 
 class ViewModel {
   constructor() {
@@ -15,11 +22,29 @@ class ViewModel {
     this.requestIcon = requestIcon;
     this.calendarIcon = calendarIcon;
     this.statsIcon = statsIcon;
+
+    this.staffId = 1;
+    this.staff = ko.observable({ avatar_url: '' });
+    this.getStaff();
   }
 
   // eslint-disable-next-line class-methods-use-this
-  reload() {
+  reload = () => {
     location.reload();
+  }
+
+  // Gets placeholder if there is no avatar
+  getAvatar = url => url || avatarUrl
+
+  // Gets the current staff account info.
+  getStaff = () => {
+    getStaff(this.staffId)
+      .then(({ data }) => {
+        this.staff(data.data);
+      })
+      .catch((error) => {
+        showError(error);
+      });
   }
 }
 
